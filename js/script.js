5f1b20644fc9ea1a270b12461db5c3da1e3987b5
Ilaria Mammucari <ilariamammucari@hotmail.it>
2020-12-16 14:42:21 +0100

    con pulsanti

--- js/script.js
+++ js/script.js
@@ -2,25 +2,47 @@
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// dichiaro variabili
-var pc;
-var utente;
-var vincitore;
+// pulsante gioca
+var pulsanteGioca = document.getElementById('gioca');
+pulsanteGioca.addEventListener('click',
+function() {
 
-// assegno un valore alle variabili
-pc = Math.floor(Math.random()*6) + 1;
-utente = Math.floor(Math.random()*6) + 1;
-vincitore = 'Hai vinto!';
+  // dichiaro variabili
+  var pc;
+  var utente;
+  var vincitore;
 
-// istruzioni condizionali
+  // assegno un valore alle variabili
+  pc = Math.floor(Math.random()*6) + 1;
+  utente = Math.floor(Math.random()*6) + 1;
+  vincitore = 'Hai vinto!';
 
-if (pc > utente) {
-  vincitore = 'Mi dispiace.. Hai perso.';
-} else if (pc == utente) {
-  vincitore = 'PAREGGIO!';
-}
+  // istruzioni condizionali
 
-//  stampo
-document.getElementById('computer').innerHTML += pc;
-document.getElementById('giocatore').innerHTML += utente;
-document.getElementById('punteggio-migliore').innerHTML = vincitore;
+  if (pc > utente) {
+    vincitore = 'Mi dispiace.. Hai perso.';
+  } else if (pc == utente) {
+    vincitore = 'PAREGGIO!';
+  }
+
+  //  stampo
+  document.getElementById('computer').innerHTML = pc;
+  document.getElementById('giocatore').innerHTML = utente;
+  document.getElementById('punteggio-migliore').innerHTML = vincitore;
+
+  // rendo visibile
+  document.getElementById('computer').className = 'show';
+  document.getElementById('giocatore').className = 'show';
+});
+
+
+
+// pulsante reset
+var pulsanteReset = document.getElementById('annulla');
+pulsanteReset.addEventListener('click',
+function() {
+
+  document.getElementById('computer').className = 'hidden';
+  document.getElementById('giocatore').className = 'hidden';
+  document.getElementById('punteggio-migliore').innerHTML = '';
+});
