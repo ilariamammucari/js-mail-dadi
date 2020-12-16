73dca0543310c49d5de404af3062ccc8f32471e9
Ilaria Mammucari <ilariamammucari@hotmail.it>
2020-12-16 14:06:04 +0100

    gioco dei dadi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-alert('ciao!');
+// Gioco dei dadi
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// dichiaro variabili
+var pc;
+var utente;
+var vincitore;
+
+// assegno un valore alle variabili
+pc = Math.floor(Math.random()*6) + 1;
+utente = Math.floor(Math.random()*6) + 1;
+vincitore = 'Hai vinto!';
+
+// istruzioni condizionali
+
+if (pc > utente) {
+  vincitore = 'Mi dispiace.. Hai perso.';
+} else if (pc == utente) {
+  vincitore = 'PAREGGIO!';
+}
+
+//  stampo
+document.getElementById('computer').innerHTML += pc;
+document.getElementById('giocatore').innerHTML += utente;
+document.getElementById('punteggio-migliore').innerHTML = vincitore;
